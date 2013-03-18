b83b13597eb5ef67c2522ce624c4f7b684ae4543
lapets <lapets@gmail.com>
2013-03-18 17:29:28 -0400

    Update JavaScript implementation with match capability.

--- uxadt.js
+++ uxadt.js
@@ -12,52 +12,76 @@ var uxadt = (function(){
   var uxadt = {};
 
   // Build a string representation of an instance.
-  uxadt.toString = function (i) {
-    var inst = i;
+  uxadt.toString = function (inst) {
     if (inst.uxadt == null)
-      return "";
-    if (inst.uxadt.t == 'v')
-      return i.uxadt.v;
-    if (inst.uxadt.t == 'c') {
-      var tmp = inst.uxadt.c
-      if (inst.uxadt.d.length != null && inst.uxadt.d.length > 0) {
-        tmp = tmp + "(";
-        for (var i = 0; i < inst.uxadt.d.length; i++) {
-          if (i > 0)
-            tmp = tmp + ", ";
-          tmp = tmp + inst.uxadt.d.toString();
-        }
-        tmp = tmp + ")";
+      return inst.toString();
+    else if (typeof inst.uxadt === 'string')
+      return inst.uxadt;
+    else {
+      for (c in inst.uxadt) { // Only to extract key.
+        var data = inst.uxadt[c], args = "";
+        if (data.length != null && data.length > 0)
+          for (var k = 0; k < data.length; k++)
+            args = (k>0?", ":"") + args + uxadt.toString(data[k]);
+        return c + (data.length > 0 ? "(" + args + ")" : "");
       }
-      return tmp;
     }
     return null;
   };
 
   // Build an instance by using a constructor.
-  uxadt.C = function (c, d) {
-    return {uxadt: {t: 'c', c: c, d: d, toString: uxadt.toString}};
+  uxadt.constructor = function (c, d) {
+    var inst = {};
+    inst[c] = (d == null ? [] : d);
+    return {uxadt: inst};
   };
 
   // Build a pattern variable instance.
-  uxadt.V = function (v) {
-    return {uxadt: {t: 'v', v: v, toString: uxadt.toString}};
+  uxadt.variable = function (v) {
+    return {uxadt: v};
   };
 
-  // Match an instance to a pattern and return
-  // a map if possible, or null otherwise.
-  uxadt.M = function (i, p) {
-    
-    // Private recursive implementation.
-    function match (i, p) {
-    
-    }
-  
+  // Match an instance to a pattern and return a map if possible,
+  // or null otherwise. By default, any attempt to merge two maps
+  // (from matched subtrees) with overlapping domains results in
+  // a non-match (i.e., null).
+  uxadt.match = function (inst, pattern) {
+    if (inst.uxadt == null || pattern.uxadt == null)
+      return null;
     var map = {};
-    
-    
-    return map;
+    if (typeof pattern.uxadt === 'string') {
+      var v = pattern.uxadt;
+      map[v] = inst;
+      return map;
+    } else {
+      for (ci in inst.uxadt) { for (cp in pattern.uxadt) { 
+        if (ci == cp) {
+          if (inst.uxadt[ci].length != pattern.uxadt[cp].length)
+            return null;
+          if (inst.uxadt[ci].length == 0)
+            return map;
+          for (var k = 0; k < inst.uxadt[ci].length; k++) {
+            var tmp = uxadt.match(inst.uxadt[ci][k], pattern.uxadt[cp][k]);
+            if (tmp == null)
+              return null;
+            for (v in tmp) {
+              if (map[v] != null) // No match if variable is already matched.
+                return null;
+              else
+                map[v] = tmp[v];
+            }
+            return map;
+          }
+        }
+      }}
+    }
+    return null;
   };
+  
+  // Concise method synonyms.
+  uxadt.C = uxadt.constructor;
+  uxadt.V = uxadt.variable;
+  uxadt.M = uxadt.match;
 
   return uxadt;
 })();
