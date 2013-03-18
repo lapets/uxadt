657a43dd2fd350dbf6a60d0925a4219ee1e62544
lapets <lapets@gmail.com>
2013-03-18 15:58:00 -0400

    Updated JavaScript implementation with toString() method.

--- uxadt.js
+++ uxadt.js
@@ -11,14 +11,37 @@
 var uxadt = (function(){
   var uxadt = {};
 
+  // Build a string representation of an instance.
+  uxadt.toString = function (i) {
+    var inst = i;
+    if (inst.uxadt == null)
+      return "";
+    if (inst.uxadt.t == 'v')
+      return i.uxadt.v;
+    if (inst.uxadt.t == 'c') {
+      var tmp = inst.uxadt.c
+      if (inst.uxadt.d.length != null && inst.uxadt.d.length > 0) {
+        tmp = tmp + "(";
+        for (var i = 0; i < inst.uxadt.d.length; i++) {
+          if (i > 0)
+            tmp = tmp + ", ";
+          tmp = tmp + inst.uxadt.d.toString();
+        }
+        tmp = tmp + ")";
+      }
+      return tmp;
+    }
+    return null;
+  };
+
   // Build an instance by using a constructor.
   uxadt.C = function (c, d) {
-    return {uxadt: {t: 'c', c: c, d: d}};
+    return {uxadt: {t: 'c', c: c, d: d, toString: uxadt.toString}};
   };
 
   // Build a pattern variable instance.
   uxadt.V = function (v) {
-    return {uxadt: {t: 'v', v: v}};
+    return {uxadt: {t: 'v', v: v, toString: uxadt.toString}};
   };
 
   // Match an instance to a pattern and return
