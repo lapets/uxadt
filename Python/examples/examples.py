ef136096a49c4bf319244acd31aef9630ce14d5a
lapets <lapets@gmail.com>
2014-06-01 20:14:38 -0400

    Updates and debugging of Python version; PHP and Python interfaces now match.

--- Python/examples/examples.py
+++ Python/examples/examples.py
@@ -8,58 +8,82 @@
 ##
 
 import uxadt
-_ = uxadt._
+_ = None
 
 #####################################################################
 ## Defining constructors in the local scope as stand-alone functions.
 ##
 
-eval(uxadt.foreval({\
+eval(uxadt._({\
+    'Red': [],\
+    'Blue': []\
+    }))
+
+eval(uxadt._({\
     'Node': [_, _],\
     'Leaf': []\
     }))
 
+c0 = Red()
 t0 = Node(Node(Leaf(), Leaf()), Node(Leaf(), Leaf()))
 t1 = Leaf()
 t2 = Node(Node(Leaf(), Leaf()), Leaf())
 
-def count(t):
+def size(t):
     return t\
         ._(Leaf(),     lambda:     1)\
-        ._(Node(_, _), lambda x,y: 1 + count(x) + count(y))\
+        ._(Node(_, _), lambda x,y: 1 + size(x) + size(y))\
         .end
 
 print("Example #1")
-print(str(t0) + " has count " + str(count(t0)) + ".")
-print(t0.data())
-t0.at('count', count(t0))
-print(str(t0.at('count')))
-print(t0.data())
+print(t0)
+print(t0.toData())
+t0.at('size', size(t0))
+print(str(t0.at('size')))
+print("")
+
+#####################################################################
+## Using alternate interface for pattern matching.
+##
+
+def width(t):
+    if t < Leaf():
+        return 1
+    elif t < Node(_, _):
+        (x, y) = t
+        return width(x) + width(y)
+
+print("Example #2")
+print(str(t0) + " has width " + str(width(t0)) + ".")
 print("")
 
 #####################################################################
 ## Defining a named object that has the constructors as its methods.
 ##
 
-Tree = uxadt.asobject({\
+Color = uxadt.qualified({\
+    'Red': [],\
+    'Blue': []\
+    })
+
+uxadt.qualified('Tree', {\
     'Node': [_, _],\
     'Leaf': []\
     })
 
+c0 = Color.Red()
 t0 = Tree.Node(Tree.Node(Tree.Leaf(), Tree.Leaf()), Tree.Leaf())
 
-def count(t):
-    return t\
-        ._(Tree.Leaf(),     lambda:     1)\
-        ._(Tree.Node(_, _), lambda x,y: 1 + count(x) + count(y))\
-        .end
+def height(t):
+    if t < Tree.Leaf():
+        return 0
+    elif t < Tree.Node(_, _):
+        (x, y) = t
+        return 1 + max(height(x), height(y))
 
-print("Example #2")
-print(str(t0) + " has count " + str(count(t0)) + ".")
-print(t0.data())
-t0.at('count', count(t0))
-print(str(t0.at('count')))
-print(t0.data())
+print("Example #3")
+print(c0)
+print(str(t0) + " has height " + str(height(t0)) + ".")
 print("")
 
 ##eof
--- Python/uxadt/uxadt.py
+++ Python/uxadt/uxadt.py
@@ -18,7 +18,7 @@
 ## supported by values.
 ##
 
-class _Matching():
+class Matching():
     candidate = None
     end = None
 
@@ -30,12 +30,9 @@ class _Matching():
             self.end = end
 
     # Matching function.
+    def _(self, p, f): return self.match(p, f)
     def match(self, p, f):
-        return _Matching(None, self.end) if self.candidate is None else self.candidate.match(p, f)
-
-    # Matching function (concise synonym).
-    def _(self, p, f):
-        return self.match(p, f)
+        return Matching(None, self.end) if self.candidate is None else self.candidate.match(p, f)
 
 #####################################################################
 ## Every UxADT value (i.e., instance of a data structure) and every
@@ -44,7 +41,9 @@ class _Matching():
 
 class Value():
     __at__ = {}
+    __last__ = None
 
+    # Constructor.
     def __init__(self, entries):
         self.__dict__.update(entries)
 
@@ -53,103 +52,102 @@ class Value():
     def equal(self, other):
         # If we are using stateful match storage, clean up
         # in case there is anything left over.
-        if hasattr(self, '__last__'): delattr(self, '__last__')
-        if hasattr(other, '__last__'): delattr(other, '__last__')
-        
+        self.__last__ = None
+
         # Compare the constructors and recursively check equality.
         for c in self.__dict__:
             for d in other.__dict__:
-                if c == d and len(self.__dict__[c]) == len(other.__dict__[d]):
-                    for i in range(0,len(self.__dict__[c])):
-                        if not self.__dict__[c][i].equal(other.__dict__[d][i]):
-                            return False
-                    return True
-                else:
-                    return False
+                if c[0:2] != '__' and d[0:2] != '__':
+                    if c == d and len(self.__dict__[c]) == len(other.__dict__[d]):
+                        for i in range(0,len(self.__dict__[c])):
+                            if not self.__dict__[c][i].equal(other.__dict__[d][i]):
+                                return False
+                        return True
+                    else:
+                        return False
         return False
 
     # Pattern matching unification algorithm.
     @staticmethod
     def unify(p, v):
-        if type(p) == str:
-            return [(p, v)]
-
         if not isinstance(p, Value):
-            return [(None, v)]
+            return [v]
 
         # Compare the constructors and recursively unify if they match.
         for c in p.__dict__:
             for d in v.__dict__:
-                if c == d and len(p.__dict__[c]) == len(v.__dict__[d]):
-                    substs = []
-                    for i in range(0,len(p.__dict__[c])):
-                        subst = Value.unify(p.__dict__[c][i], v.__dict__[d][i])
-                        if subst is None:
-                            return None
-                        substs.extend(subst)
-                    return substs
-                else:
-                    return None
+                if c[0:2] != '__' and d[0:2] != '__':
+                    if c == d and len(p.__dict__[c]) == len(v.__dict__[d]):
+                        substs = []
+                        for i in range(0,len(p.__dict__[c])):
+                            subst = Value.unify(p.__dict__[c][i], v.__dict__[d][i])
+                            if subst is None:
+                                return None
+                            substs.extend(subst)
+                        return substs
+                    else:
+                        return None
         return None
 
     # Matching function.
-    def match(self, p, f):
-        if hasattr(self, '__last__'): delattr(self, '__last__')
-        if hasattr(p, '__last__'): delattr(p, '__last__')
+    def __lt__(self, p): return self.match(p, None)
+    def _(self, p, f): return self.match(p, f)
+    def match(self, p, f = None):
+        # If we are using stateful match storage, clean up
+        # in case there is anything left over.
+        self.__last__ = None
+
         subst = Value.unify(p, self)
-        return _Matching(self, None if subst is None else f(*[v for (x,v) in subst]))
+        if f == None:
+            if subst != None and len(subst) > 0:
+                self.__last__ = subst
+            return subst != None
+        else:
+            return Matching(self, None if subst is None else f(*subst))
 
-    # Matching function (concise synonym).
-    def _(self, p, f):
-        return self.match(p, f)
-
-    # Convenient, stateful alternative for Python
-    # "if" statement blocks.
-    def __lt__(v, p):
-        subst = Value.unify(p, v)
-        if subst != None:
-            v.__last__ = [v for (x,v) in subst]
-        return subst != None
-
-    def __iter__(self):
-        if hasattr(self, '__last__'):
+    ## Last match retrieval when using stateful matching.
+    def __iter__(self): return self.matched()
+    def matched(self):
+        if self.__last__ != None:
             tmp = self.__last__
-            delattr(self, '__last__')
+            self.__last__ = None
             return iter(tmp)
-        raise NameError('UxADT error: can only traverse result of match exactly once.')
+        raise NameError('UxADT error: can only traverse result of match exactly once after a match has occurred.')
 
-    # Setting and getting labelled projections.
+    # Setting and getting labeled projections.
     def at(self, *args):
         if len(args) == 1:
             label = args[0]
             return self.__at__[label] if label in self.__at__ else None
-        if len(args) == 2:
-            label = args[0]
-            proj = args[1]
+        elif len(args) == 2:
+            [label, proj] = args
             self.__at__[label] = proj
             return self
         return self
 
     # Rendering as a native nested data structure.
-    def data(self):
+    def toData(self):
         for c in self.__dict__:
-            return {c: [v.data() for v in self.__dict__[c]]}
+            if c[0:2] != '__':
+                return {c: [v.toData() for v in self.__dict__[c]]}
 
     # Rendering as a string.
-    def __str__(self): return self.string()
-    def string(self):
+    def __str__(self): return self.toString()
+    def toString(self):
         for c in self.__dict__:
-            return c + '(' + ', '.join([v.string() for v in self.__dict__[c]]) + ')'
-        
+            if c[0:2] != '__':
+                return c + '(' + ', '.join([v.toString() for v in self.__dict__[c]]) + ')'
+
 #####################################################################
 ## Functions for defining algebraic data type constructors. There are
 ## two ways techniques supported for introducing constructors:
-##  * defining them in the local scope as stand-alone functions
-##    by passing the definition string to eval();
+##  * defining them in the local scope as stand-alone, unqualified
+##    functions by passing the definition string to eval();
 ##  * defining a named object that has the named constructors as its
 ##    only methods.
+##
 
-def foreval(sigs):
+def unqualified(sigs):
     # Since emitted code will refer to uxadt operations
     # by name, the module must be defined in the scope.
     # Find the module name and return the code to evaluate.
@@ -175,20 +173,27 @@ def foreval(sigs):
     raise NameError('UxADT error: module cannot be found in global scope. '+\
                       'Please ensure that the module is being imported correctly.')
 
-def asobject(sigs):
-    class Obj(object):
+def qualified(arg1, arg2 = None):
+    class _Type(object):
         def __init__(self, cons):
             self.__dict__.update(cons)
-    return Obj({con: eval("lambda *args: Value({'"+con+"': args})") for con in sigs})
 
-# A synonym for the default technique.
-def definition(sigs):
-    return foreval(sigs)
+    if arg2 == None:
+        sigs = arg1
+        name = '_Type' + "_".join([con for con in sigs])
+    else:
+        sigs = arg2
+        name = arg1
 
-#####################################################################
-## Useful global synonyms.
+    cls = type(name, (_Type,), {con: eval("lambda *args: Value({'"+con+"': args})") for con in sigs}) 
+    try:
+        import inspect
+        inspect.currentframe().f_back.f_globals[name] = cls
+    except: pass
+    return cls
 
-try:    _
-except: _ = None
+# Synonyms for the default technique.
+definition = unqualified
+_ = unqualified
 
 ##eof
