b23b74c68e0aa31ad08a91a66a4ff097cc151b0b
lapets <lapets@gmail.com>
2014-07-22 02:17:39 -0400

    Updates to Haskell package files.

--- Haskell/release.sh
+++ Haskell/release.sh
@@ -6,5 +6,8 @@ runhaskell Setup.lhs build
 runhaskell Setup.lhs install
 runhaskell Setup.lhs haddock
 runhaskell Setup.lhs sdist
+tar -zxvf dist/*.tar.gz
+tar --format=ustar -czf  `basename dist/*.tar.gz` uxadt-*/
+## rm -rf uxadt-*
 
 ##eof
