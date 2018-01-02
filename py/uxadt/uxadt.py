87d5a9e712279c248ebb1e05c0492b9a0429d1e8
Andrei Lapets <a@lapets.io>
2018-01-02 10:17:41 -0500

    Adjust source file header comment formatting.

--- py/uxadt/uxadt.py
+++ py/uxadt/uxadt.py
@@ -1,14 +1,12 @@
 #####################################################################
 ## 
 ## uxadt.py
+## http://uxadt.org
 ##
-##   A library that supports a universal, cross-platform embedded
-##   representation for algebraic data type (ADT) values, and a
-##   programming abstraction for operations (such as pattern
-##   matching) on algebraic data type values.
-##
-##   Web:     uxadt.org
-##   Version: 0.0.16.0
+## A library that supports a universal, cross-platform embedded
+## representation for algebraic data type (ADT) values, and a
+## programming abstraction for operations (such as pattern matching)
+## on algebraic data type values.
 ##
 ##
 
@@ -222,4 +220,4 @@ def qualified(arg1, arg2 = None):
 definition = unqualified
 _ = unqualified
 
-##eof
+## eof
